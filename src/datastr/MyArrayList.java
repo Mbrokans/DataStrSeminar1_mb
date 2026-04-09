f120d34cc2b403bcc4a0ec1bf52be7746c43eec9
Mbrokans <s25brokmart@venta.lv>
2026-04-09 16:14:32 +0300

    EDIT: homework

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -1,165 +1,32 @@
 package datastr;
 
+
+
 public class MyArrayList {
-	//1. mainigie
-	private char[] list;
-	private final int DEFAULT_SIZE =8; // pec noklusejuma bus 8 sunu masivs
-	private int size = DEFAULT_SIZE;
-	private int howManyElements = 0;
-	//2. konstrokturi
-	public MyArrayList() {
-		list= new char[size];
+
+	    public static void insertionSort(int[] arr) {
+	        for (int i = 1; i < arr.length; i++) {
+	            int key = arr[i];     
+	            int j = i - 1;
+
+	            while (j >= 0 && arr[j] > key) {
+	                arr[j + 1] = arr[j];
+	                j--;
+	            }
+
+	            arr[j + 1] = key; 
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        int[] numbers = {5, 2, 18, 1, 13, 6, 9, 10, 11, -1};
+
+	        insertionSort(numbers);
+
+	        System.out.println("Sorted list:");
+	        for (int num : numbers) {
+	            System.out.print(num + " ");
+	        }
+	    }
 	}
-	public MyArrayList(int inputSize) {
-		if(inputSize>0) {
-			size=inputSize;
-		}
-			
-		list= new char[inputSize];
-	}
-	
-	//3. pamatalgoritmu funkcijas
-	public boolean isEmpty() {
-		//garais if variants
-		if(howManyElements==0) {
-			return true; // tukss
-		}
-		else {
-			return false;// nav tukss
-		}
-		// isais if varinats
-//		return (howManyElements==0) ? true:false;
-	}
-	public boolean isFull() {
-		return (howManyElements==size);
-	}
-	private void resize() {
-		int largeSize = (howManyElements<100)? size*2: (int)(size*1.5);
-		char[] largeList = new char[largeSize];
-		for(int i=0; i<howManyElements;i++) {
-			largeList[i] = list[i];
-		}
-		list=largeList;
-		System.gc();// izdzesis no RAM atminas mazo masiva
-		size= largeSize;
-		
-	}
-	public void add(char element) {
-		if(isFull()) {
-			resize();
-		}
-		list[howManyElements]= element;
-		howManyElements++;
-		
-		
-	}
-	// pievieno elemntu
-	public void add(char element, int index) throws Exception {
-		if(index<0) {
-			throw (new Exception("Nevar pievienot elemntu jo index ir negativs"));
-			
-		}
-		if(index> howManyElements) {
-			throw new Exception("Nevar pievienot elementu, jo index ir lielaks par elemntu");
-		}
-		if(isFull()) {
-			resize();
-		}
-		if(index == howManyElements) {
-			add(element);
-		}
-		else {
-			if(isFull()) {
-				resize();
-			}
-			for(int i = howManyElements;i>index;i--) {
-				list[i]= list[i-1];
-			}
-			list[index]= element;
-			howManyElements++;
-		}
-		
-	}
-	// iznem elemntu
-	public void remove(int index) throws Exception{
-		if(isEmpty()) {
-			throw (new Exception("saraksts ir tukss"));
-		}
-		if(index<0) {
-			throw (new Exception("Nevar izdzest elemntu"));
-		}
-		if(index>howManyElements) {
-			throw (new Exception("nevar izdzest elemntu"));
-		}
-		for(int i = index; i<howManyElements-1;i++) {
-			list[i]= list[i+1];
-		}
-		list[howManyElements-1]=' ';
-		howManyElements--;
-	}
-	// izprinte
-	public void print()throws Exception {
-		if(isEmpty()) {
-			throw (new Exception("Saraksts ir tukss un to nevar izprintet"));
-		}
-		for( int i=0;i<howManyElements;i++) {
-			System.out.print(list[i]+" ");
-		}
-		System.out.println();
-	}
-	//parada elemntu
-	public char get(int index)throws Exception {
-		if(isEmpty()) {
-			throw (new Exception("Nevar paradit elementu"));
-		}
-		if(index<0) {
-			throw (new Exception("Nevar paradit mazaku elementu"));
-		}
-		if(index>howManyElements) {
-			throw (new Exception("Nevar paradit lielaku elemntu"));
-		}
-		return list[index];
-	}
-	// meklesana
-	public int search(char element)throws Exception {
-		if(isEmpty()) {
-			throw (new Exception("saraksrs ir tukss nevar sameklet elementu"));
-		}
-		for( int i=0;i<howManyElements;i++) {
-			if(list[i]==element) {
-				return i;
-			}
-		}
-		
-		throw (new Exception("mekletais elements nav atrasts"));
-	}
-	public char getNextElement(char element) throws Exception{
-		int indexOfSearch = search(element);
-		if(indexOfSearch==howManyElements-1) { // ja mekletais elements ir pedejais
-			throw (new Exception("jsus ievaditais elements ir atrasts ka pedejais un aiz to neka nav"));
-		}
-		int indexForNextElement = indexOfSearch+1;
-		return list[indexForNextElement];
-	}
-	public void sort() {
-		for(int i=0;i<howManyElements;i++) {
-			for(int j=0; j<howManyElements;j++) {
-				if(list[i]<list[j]) {
-					char temp= list[i];
-					list[i]=list[j];
-					list[j]=temp;
-				}
-			}
-		}
-	}
-	public void MakeEmpty() {
-		list=null;
-		System.gc();
-		howManyElements=0;
-		size= DEFAULT_SIZE;
-		list = new char[size];
-	}
-	
-	
-	
-}
+
--- src/service/mainservice.java
+++ src/service/mainservice.java
@@ -3,32 +3,32 @@ package service;
 import datastr.MyArrayList;
 
 public class mainservice {
-	public static void main(String[] args) {
-		MyArrayList symbols = new MyArrayList(2);
-		symbols.add('a');//a
-		symbols.add('b');//a b
-		symbols.add('c');//a b c (automatiski bus resize izpildijies)
-		try {
-			symbols.print();//a b c
-			symbols.add('Z',1);// a Z b c
-			symbols.print();
-			symbols.remove(2);// izdzest b
-			symbols.print();
-			System.out.println(symbols.get(1));
-			
-			System.out.println(symbols.search('c')); // 2. pozicija
-			System.out.println(symbols.getNextElement('a'));// Z
-			symbols.sort();
-			symbols.print();
-			
-			symbols.MakeEmpty();// viss tiek dzests
-			symbols.add('W');
-			symbols.print();
-		}
-		catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		
-		
-	}
+//	public static void main(String[] args) {
+//		MyArrayList symbols = new MyArrayList(2);
+//		symbols.add('a');//a
+//		symbols.add('b');//a b
+//		symbols.add('c');//a b c (automatiski bus resize izpildijies)
+//		try {
+//			symbols.print();//a b c
+//			symbols.add('Z',1);// a Z b c
+//			symbols.print();
+//			symbols.remove(2);// izdzest b
+//			symbols.print();
+//			System.out.println(symbols.get(1));
+//			
+//			System.out.println(symbols.search('c')); // 2. pozicija
+//			System.out.println(symbols.getNextElement('a'));// Z
+//			symbols.sort();
+//			symbols.print();
+//			
+//			symbols.MakeEmpty();// viss tiek dzests
+//			symbols.add('W');
+//			symbols.print();
+//		}
+//		catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//		
+//		
+//	}
 }
