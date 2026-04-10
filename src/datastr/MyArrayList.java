6a27209e2e28df5d3acde36d34bce7b217c4d133
Mbrokans <s25brokmart@venta.lv>
2026-04-10 15:13:50 +0300

    EDIT; added a student class

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -1,15 +1,15 @@
 package datastr;
 
-
+import model.student;
 
 public class MyArrayList {
+	
+	public static void insertionSort(int[] arr) {
+	   for (int i = 1; i < arr.length; i++) {
+	           int key = arr[i];     
+	           int j = i - 1;
 
-	    public static void insertionSort(int[] arr) {
-	        for (int i = 1; i < arr.length; i++) {
-	            int key = arr[i];     
-	            int j = i - 1;
-
-	            while (j >= 0 && arr[j] > key) {
+	           while (j >= 0 && arr[j] > key) {
 	                arr[j + 1] = arr[j];
 	                j--;
 	            }
@@ -18,14 +18,13 @@ public class MyArrayList {
 	        }
 	    }
 
-	    public static void main(String[] args) {
-	        int[] numbers = {5, 2, 18, 1, 13, 6, 9, 10, 11, -1};
+	public static void main(String[] args) {
+	    int[] numbers = {5, 2, 18, 1, 13, 6, 9, 10, 11, -1};
+	     insertionSort(numbers);
 
-	        insertionSort(numbers);
-
-	        System.out.println("Sorted list:");
-	        for (int num : numbers) {
-	            System.out.print(num + " ");
+	     System.out.println("Sorted list:");
+	     for (int num : numbers) {
+	     System.out.print(num + " ");
 	        }
 	    }
 	}
