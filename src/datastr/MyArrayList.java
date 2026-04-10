818ed5c4e7ea4685b13194ee73f5fa4b017448c3
Mbrokans <s25brokmart@venta.lv>
2026-04-10 21:36:25 +0300

    EDIT:added a student list now just need to fix the print part

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -1,7 +1,5 @@
 package datastr;
 
-import model.student;
-
 public class MyArrayList {
 	
 	public static void insertionSort(int[] arr) {
@@ -17,14 +15,32 @@ public class MyArrayList {
 	            arr[j + 1] = key; 
 	        }
 	    }
+	public static void insertionSort1(String[] arr) {
+		for (int i=1; i<arr.length;i++) {
+			String key = arr[i];
+			int j = i-1;
+			
+			while(j>=0 && arr[j].compareTo(key)>0) {
+				arr[j+1]= arr[j];
+				j--;
+			}
+			arr[j+1]=key;
+		}
+	}
 
 	public static void main(String[] args) {
 	    int[] numbers = {5, 2, 18, 1, 13, 6, 9, 10, 11, -1};
+	    String[] studenti = {"Maris", "Alise", "Talivaldis", "Benrijs", "Darta"};
 	     insertionSort(numbers);
+	     insertionSort1(studenti);
 
 	     System.out.println("Sorted list:");
 	     for (int num : numbers) {
 	     System.out.print(num + " ");
+	     System.out.println("Sorted name list: ");
+	     for(String names : studenti) {
+	    	 System.out.println(names+" ");
+	     }
 	        }
 	    }
 	}
